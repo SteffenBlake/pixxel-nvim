de1f851a9f92bff118af66cb4643ef8c22248b6e
steffen.cole.blake@gmail.com <steffen.cole.blake@gmail.com>
2026-01-13 23:40:28 -0700

    Copilot Bugfix

--- lua/copilot-config/init.lua
+++ lua/copilot-config/init.lua
@@ -6,11 +6,11 @@ function M.setup(ctx)
             "CopilotC-Nvim/CopilotChat.nvim",
             dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope.nvim', },
             opts = {
-                model = 'Claude Sonnet 4.5',
+                model = 'claude-sonnet-4.5',
                 temperature = 0.1,
                 window = {
                     layout = 'vertical',
-                    width = 0.5,
+                    width = 0.3,
                 },
                 auto_insert_mode = true,
             }
@@ -22,12 +22,12 @@ function M.run(_)
     wk.add({
         { "<leader>lt", ":CopilotChatToggle<CR>", desc = "[t]oggle Copilot chat", mode = { 'n' } },
         { "<leader>ls", ":CopilotChatStop<CR>", desc = "[s]top Copilot chat output", mode = { 'n' } },
-        { "<leader>lp", ":CopilotChatPrompts<CR>", desc = "View [p]romt templates", mode = { 'n' } },
-        { "<leader>le", ":CopilotChatExplain<CR>", desc = "[e]xplain current selection", mode = { 'n', 'i' } },
-        { "<leader>lr", ":CopilotChatReview<CR>", desc = "[r]eview current selection", mode = { 'n', 'i' } },
-        { "<leader>lf", ":CopilotChatFix<CR>", desc = "[f]ix current selection", mode = { 'n', 'i' } },
-        { "<leader>lo", ":CopilotChatOptimize<CR>", desc = "[o]ptimize current selection", mode = { 'n', 'i' } },
-        { "<leader>ld", ":CopilotChatDocs<CR>", desc = "[d]ocument current selection", mode = { 'n', 'i' } },
+        { "<leader>lp", ":CopilotChatPrompts<CR>", desc = "View [p]romt templates", mode = { 'n', 'v' } },
+        { "<leader>le", ":CopilotChatExplain<CR>", desc = "[e]xplain current selection", mode = { 'n', 'v' } },
+        { "<leader>lr", ":CopilotChatReview<CR>", desc = "[r]eview current selection", mode = { 'n', 'v' } },
+        { "<leader>lf", ":CopilotChatFix<CR>", desc = "[f]ix current selection", mode = { 'n', 'v' } },
+        { "<leader>lo", ":CopilotChatOptimize<CR>", desc = "[o]ptimize current selection", mode = { 'n', 'v' } },
+        { "<leader>ld", ":CopilotChatDocs<CR>", desc = "[d]ocument current selection", mode = { 'n', 'v' } },
     })
 end
 
