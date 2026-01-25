62b01441a26b2cc4b0ff008115a9364071d6d2e1
steffen.cole.blake@gmail.com <steffen.cole.blake@gmail.com>
2026-01-25 14:08:01 -0700

    treesitter fix
    Treesitter fix

--- lua/lsp-config/treesitter-config.lua
+++ lua/lsp-config/treesitter-config.lua
@@ -4,9 +4,6 @@ function M.setup(ctx)
     table.insert(ctx.lazy, {
         -- Highlight, edit, and navigate code
         'nvim-treesitter/nvim-treesitter',
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter-textobjects',
-        },
         build = ':TSUpdate',
     })
 
@@ -14,79 +11,14 @@ function M.setup(ctx)
 end
 
 function M.run(ctx)
-    local treeSitterConfigs = require('nvim-treesitter.configs')
+    local treeSitter = require('nvim-treesitter');
+    treeSitter.install(ctx.treesitter_languages);
 
-    vim.defer_fn(function()
-        treeSitterConfigs.setup {
-            -- Add languages to be installed here that you want installed for treesitter
-            ensure_installed = ctx.treesitter_languages,
-
-            -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-            auto_install = true,
-
-            highlight = { enable = true },
-            indent = { enable = true },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = '<c-space>',
-                    node_incremental = '<c-space>',
-                    scope_incremental = '<c-s>',
-                    node_decremental = '<M-space>',
-                },
-            },
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-                    keymaps = {
-                        -- You can use the capture groups defined in textobjects.scm
-                        ['aa'] = '@parameter.outer',
-                        ['ia'] = '@parameter.inner',
-                        ['af'] = '@function.outer',
-                        ['if'] = '@function.inner',
-                        ['ac'] = '@class.outer',
-                        ['ic'] = '@class.inner',
-                    },
-                },
-                move = {
-                    enable = true,
-                    set_jumps = true, -- whether to set jumps in the jumplist
-                    goto_next_start = {
-                        [']m'] = '@function.outer',
-                        [']]'] = '@class.outer',
-                    },
-                    goto_next_end = {
-                        [']M'] = '@function.outer',
-                        [']['] = '@class.outer',
-                    },
-                    goto_previous_start = {
-                        ['[m'] = '@function.outer',
-                        ['[['] = '@class.outer',
-                    },
-                    goto_previous_end = {
-                        ['[M'] = '@function.outer',
-                        ['[]'] = '@class.outer',
-                    },
-                },
-                swap = {
-                    enable = true,
-                    swap_next = {
-                        ['<leader>a'] = '@parameter.inner',
-                    },
-                    swap_previous = {
-                        ['<leader>A'] = '@parameter.inner',
-                    },
-                },
-            },
-        }
-
-        -- Tweaks to make markdown play nice
-        vim.api.nvim_set_hl(0, 'mkdBold', { bold = true })
-        vim.api.nvim_set_hl(0, 'mkdItalic', { italic = true })
-        vim.api.nvim_set_hl(0, 'markdownBold', { bold = true })
-        vim.api.nvim_set_hl(0, '@markup.italic.markdown_inline', { italic = true })
-    end, 0)
+    -- Tweaks to make markdown play nice
+    vim.api.nvim_set_hl(0, 'mkdBold', { bold = true })
+    vim.api.nvim_set_hl(0, 'mkdItalic', { italic = true })
+    vim.api.nvim_set_hl(0, 'markdownBold', { bold = true })
+    vim.api.nvim_set_hl(0, '@markup.italic.markdown_inline', { italic = true })
 end
 
 return M
